009ee331fd99ebb3e8dec2561fb4e8a860d383f7
Vitor Galdino <httpvittor@gmail.com>
2023-04-04 17:35:09 -0300

    feat: create "Group" model

--- groups/models.py
+++ groups/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Group(models.Model):
+    scientific_name = models.CharField(max_length=50, unique=True),
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __repr__(self) -> str:
+        return f'<[{self.id}] Group - {self.scientific_name}>'
