21b05991a6d8d67285f004518026252bab942dfe
Vitor Galdino <httpvittor@gmail.com>
2023-04-05 12:40:39 -0300

    feat: create serializer from "Pet" model

---
+++ pets/serializers.py
@@ -0,0 +1,18 @@
+from rest_framework import serializers
+
+from groups.serializers import GroupSerializer
+from traits.serializers import TraitSerializer
+
+from .models import Genres
+
+
+class PetSerializer(serializers.Serializer):
+    id = serializers.IntegerField(read_only=True)
+    name = serializers.CharField(max_length=50)
+    age = serializers.IntegerField()
+    weight = serializers.FloatField()
+    sex = serializers.ChoiceField(
+        choices=Genres.choices, default=Genres.DEFAULT
+    )
+    group = GroupSerializer()
+    traits = TraitSerializer(many=True)
