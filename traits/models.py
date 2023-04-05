3a44e62cca9838ade698ec43bd39403e19cb5cac
Vitor Galdino <httpvittor@gmail.com>
2023-04-04 23:56:11 -0300

    feat: include "created_at" attribute

--- traits/models.py
+++ traits/models.py
@@ -4,6 +4,7 @@ from django.db import models
 class Trait(models.Model):
     name = models.CharField(max_length=20, unique=True)
     pets = models.ManyToManyField('pets.Pet', related_name='traits')
+    created_at = models.DateTimeField(auto_now_add=True)
 
     def __repr__(self) -> str:
         return f'<[{self.id}] Trait - {self.name}>'
