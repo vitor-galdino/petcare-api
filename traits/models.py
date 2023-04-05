644211ff5b16cf21315705bc295d26d496f5acbd
Vitor Galdino <httpvittor@gmail.com>
2023-04-04 21:49:47 -0300

    feat: create "Trait" model

--- traits/models.py
+++ traits/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class Trait(models.Model):
+    name = models.CharField(max_length=20, unique=True)
+    pets = models.ManyToManyField('pets.Pet', related_name='traits')
+
+    def __repr__(self) -> str:
+        return f'<[{self.id}] Pet - {self.name}>'
