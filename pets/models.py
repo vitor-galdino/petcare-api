87026287f345789b5bc2780b3065fb263dbff83d
Vitor Galdino <httpvittor@gmail.com>
2023-04-04 21:35:43 -0300

    feat: created "Pet" model

--- pets/models.py
+++ pets/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+
+class Genres(models.TextChoices):
+    MALE = 'Male'
+    FEMALE = 'Female'
+    DEFAULT = 'Not Informed'
+
+
+class Pet(models.Model):
+    name = models.CharField(max_length=50)
+    age = models.IntegerField()
+    weight = models.FloatField()
+    sex = models.CharField(
+        max_length=20, choices=Genres.choices, default=Genres.DEFAULT
+    )
+    group = models.ForeignKey(
+        'groups.Group', on_delete=models.PROTECT, related_name='pets'
+    )
+
+    def __repr__(self) -> str:
+        return f'<[{self.id}] Pet - {self.name}>'
