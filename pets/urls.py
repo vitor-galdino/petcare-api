579f451d6560c4ee5187b5d2fe67d4e2ecd0010c
Vitor Galdino <httpvittor@gmail.com>
2023-04-05 21:36:58 -0300

    feat: add new pet route handled with id

--- pets/urls.py
+++ pets/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views import PetView
+from .views import PetView, PetDetailView
 
 urlpatterns = [
     path('pets/', PetView.as_view()),
+    path('pets/<int:pet_id>/', PetDetailView.as_view()),
 ]
